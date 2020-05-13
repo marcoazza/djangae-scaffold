b0aed945d5deee15f8c8ea5878bde04768f00503
Marco Azzalin <marco.azzalin@potatolondon.com>
2020-05-13 16:46:30 +0200

    Remove unsued files and functions

--- project_name/boot.py
+++ project_name/boot.py
@@ -1,36 +1,3 @@
-import sys
-from os.path import dirname, abspath, join, exists
-
-PROJECT_DIR = dirname(dirname(abspath(__file__)))
-SITEPACKAGES_DIR = join(PROJECT_DIR, "sitepackages")
-DEV_SITEPACKAGES_DIR = join(SITEPACKAGES_DIR, "dev")
-PROD_SITEPACKAGES_DIR = join(SITEPACKAGES_DIR, "prod")
-APPENGINE_DIR = join(DEV_SITEPACKAGES_DIR, "google_appengine")
-
-
-
-def fix_path(include_dev_libs_path=False):
-    """ Insert libs folder(s) and SDK into sys.path. The one(s) inserted last take priority. """
-    if include_dev_libs_path:
-        if exists(APPENGINE_DIR) and APPENGINE_DIR not in sys.path:
-            sys.path.insert(1, APPENGINE_DIR)
-
-        if DEV_SITEPACKAGES_DIR not in sys.path:
-            sys.path.insert(1, DEV_SITEPACKAGES_DIR)
-
-    if SITEPACKAGES_DIR not in sys.path:
-        sys.path.insert(1, PROD_SITEPACKAGES_DIR)
-
-
-def register_custom_checks():
-    from . import checks
-    from django.core.checks import register, Tags
-    register(checks.check_csp_sources_not_unsafe, Tags.security, deploy=True)
-    register(checks.check_session_csrf_enabled, Tags.security)
-    register(checks.check_csp_is_not_report_only, Tags.security)
-    register(checks.check_cached_template_loader_used, Tags.caches, deploy=True)
-
-
 def get_app_config(db):
     """Returns the application configuration, creating it if necessary."""
     from django.utils.crypto import get_random_string
@@ -39,6 +6,7 @@ def get_app_config(db):
     from google.cloud.exceptions import Unauthorized
     import os
     import requests
+
     allowed_chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 
     gclient = datastore.Client(
